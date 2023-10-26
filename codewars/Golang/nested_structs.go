8c62b29105f97dab072ce6b985c0d0d3eb804447
Marucha <marucha.webdev@yahoo.com>
2023-10-26 12:36:51 +0300

    feat: Nested Structs

---
+++ codewars/Golang/nested_structs.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+)
+
+type messageToSend struct {
+	message   string
+	sender    user
+	recipient user
+}
+
+type user struct {
+	name   string
+	number int
+}
+
+// function that confirms that a user doesn't have default values
+func checkNil (val user) bool {
+	return val.name != "" && val.number != 0
+}
+
+func canSendMessage(mToSend messageToSend) bool {
+	// check whether there are empty values in the message
+	return checkNil(mToSend.sender) && checkNil(mToSend.recipient)
+}
+
+// don't touch below this line
+
+func test(mToSend messageToSend) {
+	fmt.Printf(`sending "%s" from %s (%v) to %s (%v)...`,
+		mToSend.message,
+		mToSend.sender.name,
+		mToSend.sender.number,
+		mToSend.recipient.name,
+		mToSend.recipient.number,
+	)
+	fmt.Println()
+	if canSendMessage(mToSend) {
+		fmt.Println("...sent!")
+	} else {
+		fmt.Println("...can't send message")
+	}
+	fmt.Println("====================================")
+}
+
+func main() {
+	test(messageToSend{
+		message: "you have an appointment tommorow",
+		sender: user{
+			name:   "Brenda Halafax",
+			number: 16545550987,
+		},
+		recipient: user{
+			name:   "Sally Sue",
+			number: 19035558973,
+		},
+	})
+	test(messageToSend{
+		message: "you have an event tommorow",
+		sender: user{
+			number: 16545550987,
+		},
+		recipient: user{
+			name:   "Suzie Sall",
+			number: 0,
+		},
+	})
+	test(messageToSend{
+		message: "you have an party tommorow",
+		sender: user{
+			name:   "Njorn Halafax",
+			number: 16545550987,
+		},
+		recipient: user{
+			name:   "Sally Sue",
+			number: 19035558973,
+		},
+	})
+	test(messageToSend{
+		message: "you have a birthday tommorow",
+		sender: user{
+			name:   "Eli Halafax",
+			number: 0,
+		},
+		recipient: user{
+			name:   "Whitaker Sue",
+			number: 19035558973,
+		},
+	})
+}
